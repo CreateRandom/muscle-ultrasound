e6693a460e042dbf988da63e431c8eced14cada4
CreateRandom <klausmichaellux@googlemail.com>
2020-06-26 18:04:27 +0200

    bump reqs, missing utility func

---
+++ loading/loading_utils.py
@@ -0,0 +1,21 @@
+from loading.loaders import SetSpec
+
+def make_set_specs(umc_data_path, umc_img_root, jhu_data_path, jhu_img_root):
+    # for each device, we have a test, val and a test set
+    device_mapping = {'ESAOTE_6100': 'umc', 'GE_Logiq_E': 'jhu', 'Philips_iU22': 'umc'}
+    device_splits = {'ESAOTE_6100': ['train', 'val', 'test'], 'GE_Logiq_E': ['im_muscle_chart'],
+                     'Philips_iU22': ['train', 'val', 'test']}
+
+    label_paths = {'umc': umc_data_path, 'jhu': jhu_data_path}
+    img_root_paths = {'umc': umc_img_root, 'jhu': jhu_img_root}
+    set_specs = {}
+    for device, dataset_type in device_mapping.items():
+        # get the splits
+        splits = device_splits[device]
+        label_path = label_paths[dataset_type]
+        img_root_path = img_root_paths[dataset_type]
+        for split in splits:
+            spec_name = device + '_' + split
+
+            set_specs[spec_name] = SetSpec(device, dataset_type, split, label_path, img_root_path)
+    return set_specs
